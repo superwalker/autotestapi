f7e9f2f0a7570768d789fe1b7bf84d2ddcbfd829
walkerchow <371685029@qq.com>
2021-12-22 14:42:06 +0800

    1.增加控制判断

--- testcase/testAPI.py
+++ testcase/testAPI.py
@@ -51,7 +51,7 @@ class MF_API(unittest.TestCase):
     def tearDown(self):
         # sqldb.close(self)
         pass
-    @classmethod
+
     def ParamsAnalysis(self,params):
 
         where_datas = {'merchant_id': '31'}
@@ -127,80 +127,132 @@ class MF_API(unittest.TestCase):
         # # 将结果进行反序列化
         self.result = re.json()
 
-
-        ''' 获取请求返回值第一条数据'''
-        resp=self.result['data'][0]
-        # print(resp['status'])
-
-        # 处理病症描述字段
-        disease_name = []
-        for v in resp['disease']:
-            disease_name.append(v['disease_name'])
-        disease_name = ','.join(disease_name)
-
-        '''获取数据库返回第一条数据'''
-        # '''实例化数据库'''
-        sqldb = DB()
-        sql = data['headers']
-        dbresult=sqldb.executesql(sql)
-        dbres=dbresult[0]
-        dbres['created_at']=str(dbres['created_at'])
+        respon=self.result['data']
 
 
-        '''获取请求返回的所有id'''
-        resid = []
-        for k in self.result['data']:
-            resid.append(k['id'])
+        if respon :
+
+            ''' 获取请求返回值第一条数据'''
+            resp = self.result['data'][0]
+            # 处理疾病名称
+            disease_name = []
+            for v in resp['disease']:
+                disease_name.append(v['disease_name'])
+            disease_name = ','.join(disease_name)
+
+            '''获取请求返回的所有id'''
+            resid = []
+            for k in self.result['data']:
+                resid.append(k['id'])
+
+            '''获取请求返回数据总数'''
+            total = self.result['total']
+
+            '''获取数据库返回第一条数据'''
+            # '''实例化数据库'''
+            sqldb = DB()
+            sql = data['headers']
+            dbresult = sqldb.executesql(sql)
+            dbres = dbresult[0]
+            dbres['created_at'] = str(dbres['created_at'])
+
+            '''获取第一页数据库id'''
+            table_name = 'mf_order'
+            select_datas = ['id']
+            '''查询条件解析传值'''
+            aa = self.ParamsAnalysis(data['params'])
+            dbid = sqldb.MultiQuery(table_name, select_datas, **aa)
+
+            resdbid = []
+            for k in dbid:
+                resdbid.append(k['id'])
+            # print(resdbid)
+
+            '''获取数据库查询总数据量'''
+            select_datas1 = ['count(*)']
+            aa = self.ParamsAnalysis(data['params'])
+            dbtotal = sqldb.MultiQuery(table_name, select_datas1, **aa)
+            dbtotal = dbtotal[0]['count(*)']
+            # print(dbtotal)
+
+
+            '''第一条数据字段和数据库第一条数据断言'''
+            self.assertEqual(resp['id'], dbres['id'], "接口返回id:%d ,数据库返回id:%d" % (resp['id'], dbres['id']))
+            self.assertEqual(resp['created_at'], dbres['created_at'],"接口返回问诊时间:%s ,数据库返回问诊时间:%s" % (resp['created_at'], dbres['created_at']))
+            self.assertEqual(resp['diag_id'], dbres['diag_id'],"接口返回问诊单号diag_id:%s ,数据库返回问诊单号diag_id:%s" % (resp['diag_id'], dbres['diag_id']))
+            self.assertEqual(resp['shop_name'], dbres['shop_name'],"接口返回门店名称shop_name:%s ,数据库返回门店名称shop_name:%s" % (resp['shop_name'], dbres['shop_name']))
+            self.assertEqual(resp['user_drugs_name'], dbres['user_drugs_name'],"接口返回【用药人user_drugs_name】:%s ,数据库返回【用药人user_drugs_name】:%s" % (resp['user_drugs_name'], dbres['user_drugs_name']))
+            self.assertEqual(resp['card_no'], dbres['card_no'],"接口返回【身份证card_no】:%s ,数据库返回身份证card_no:%s" % (resp['card_no'], dbres['card_no']))
+            self.assertEqual(resp['phone'], dbres['phone'],"接口返回【手机号phone】:%s ,数据库返回【手机号phone】:%s" % (resp['phone'], dbres['phone']))
+            self.assertEqual(resp['user_drug']['age'], dbres['age'],"接口返回【年龄age】:%s ,数据库返回【年龄age】:%s" % (resp['user_drug']['age'], dbres['age']))
+            self.assertEqual(resp['user_drug']['sex'], dbres['sex'],"接口返回【性别sex】:%s ,数据库返回【性别sex】:%s" % (resp['user_drug']['sex'], dbres['sex']))
+            self.assertEqual(disease_name, dbres['disease_name'],"接口返回【病情描述disease_name】:%s ,数据库返回【病情描述disease_name】:%s" % (disease_name, dbres['disease_name']))
+            self.assertEqual(resp['status'], dbres['status'],"接口返回【订单状态status】:%s ,数据库返回【订单状态status】:%s" % (resp['status'], dbres['status']))
+            self.assertEqual(resp['remark'], dbres['remark'],"接口返回【备注remark】:%s ,数据库返回【备注remark】:%s" % (resp['remark'], dbres['remark']))
+            print('首条数据返回值：')
+            print(resp)
+            print('数据库首条数据返回值：')
+            print(dbres)
+
+            '''请求返回code断言'''
+            self.assertEqual(re.status_code, 200, "接口返回【状态码】:%s ,预期返回【状态码】:%s" % (re.status_code, 200))
+            print('请求返回code:' + str(re.status_code) )
+
+            '''请求第一页所有id 和数据库前10条id断言'''
+            self.assertEqual(resid, resdbid, "接口返回所有【id】:%s ,数据库返回所有【id】:%s" % (resid, resdbid))
+            print('请求第一页所有id:')
+            print (resid)
+            print('数据库第一页id:')
+            print(resdbid)
+
+            '''请求返回数据总数和数据库数据总数断言'''
+            self.assertEqual(total, dbtotal, "接口返回所有【id】:%s ,数据库返回所有【id】:%s" % (total, dbtotal))
+            print('请求返回数据总数:')
+            print(total)
+            print('数据库数据总数:')
+            print(dbtotal)
+
+
+        else:
+
+            '''获取请求返回数据总数'''
+            total = self.result['total']
+
+            sqldb = DB()
+            sql = data['headers']
+            dbresult = sqldb.executesql(sql)
+
+            '''获取数据库查询总数据量'''
+            table_name = 'mf_order'
+            select_datas1 = ['count(*)']
+            aa = self.ParamsAnalysis(data['params'])
+            dbtotal = sqldb.MultiQuery(table_name, select_datas1, **aa)
+            dbtotal = dbtotal[0]['count(*)']
+
+            '''list 转化为 tuple 进行断言'''
+            respon=tuple(respon)
+
+            '''无数据查询出断言'''
+            self.assertEqual(respon,dbresult,"接口返回为空值：%s,数据库返回为空值：%s" % (respon,dbresult))
+
+            '''请求返回code断言'''
+            self.assertEqual(re.status_code, 200, "接口返回【状态码】:%s ,预期返回【状态码】:%s" % (re.status_code, 200))
+            print('请求返回code:' + str(re.status_code) )
+
+            '''请求返回数据总数和数据库数据总数断言'''
+            self.assertEqual(total, dbtotal, "接口返回所有【id】:%s ,数据库返回所有【id】:%s" % (total, dbtotal))
+            print('请求返回数据总数:')
+            print(total)
+            print('数据库数据总数:')
+            print(dbtotal)
 
 
 
 
-        '''获取第一页数据库id'''
-        table_name='mf_order'
-        select_datas=['id']
-        aa= self.ParamsAnalysis(data['params'])
-
-        dbid=sqldb.MultiQuery(table_name,select_datas,**aa)
-
-        resdbid=[]
-        for k in dbid:
-            resdbid.append(k['id'])
-        print(resdbid)
 
 
-        '''获取数据库查询总数据量'''
-        select_datas1 = ['count(*)']
-        aa= self.ParamsAnalysis(data['params'])
-        dbtotal = sqldb.MultiQuery(table_name, select_datas1, **aa)
-        dbtotal = dbtotal[0]['count(*)']
-        print(dbtotal)
-
-        '''获取请求返回数据总数'''
-        total=self.result['total']
 
 
-        '''第一条数据字段和数据库第一条数据断言'''
-        self.assertEqual(resp['id'],dbres['id'],"接口返回id:%d ,数据库返回id:%d" % (resp['id'],dbres['id']))
-        self.assertEqual(resp['created_at'], dbres['created_at'], "接口返回问诊时间:%s ,数据库返回问诊时间:%s" % (resp['created_at'], dbres['created_at']))
-        self.assertEqual(resp['diag_id'], dbres['diag_id'], "接口返回问诊单号diag_id:%s ,数据库返回问诊单号diag_id:%s" % (resp['diag_id'], dbres['diag_id']))
-        self.assertEqual(resp['shop_name'], dbres['shop_name'], "接口返回门店名称shop_name:%s ,数据库返回门店名称shop_name:%s" % (resp['shop_name'], dbres['shop_name']))
-        self.assertEqual(resp['user_drugs_name'], dbres['user_drugs_name'], "接口返回【用药人user_drugs_name】:%s ,数据库返回【用药人user_drugs_name】:%s" % (resp['user_drugs_name'], dbres['user_drugs_name']))
-        self.assertEqual(resp['card_no'], dbres['card_no'], "接口返回【身份证card_no】:%s ,数据库返回身份证card_no:%s" % (resp['card_no'], dbres['card_no']))
-        self.assertEqual(resp['phone'], dbres['phone'], "接口返回【手机号phone】:%s ,数据库返回【手机号phone】:%s" % (resp['phone'], dbres['phone']))
-        self.assertEqual(resp['user_drug']['age'], dbres['age'],"接口返回【年龄age】:%s ,数据库返回【年龄age】:%s" % (resp['user_drug']['age'], dbres['age']))
-        self.assertEqual(resp['user_drug']['sex'], dbres['sex'], "接口返回【性别sex】:%s ,数据库返回【性别sex】:%s" % (resp['user_drug']['sex'], dbres['sex']))
-        self.assertEqual(disease_name, dbres['disease_name'], "接口返回【病情描述disease_name】:%s ,数据库返回【病情描述disease_name】:%s" % (disease_name, dbres['disease_name']))
-        self.assertEqual(resp['status'], dbres['status'], "接口返回【订单状态status】:%s ,数据库返回【订单状态status】:%s" % (resp['status'], dbres['status']))
-        self.assertEqual(resp['remark'], dbres['remark'], "接口返回【备注remark】:%s ,数据库返回【备注remark】:%s" % (resp['remark'], dbres['remark']))
-
-        '''请求返回code断言'''
-        self.assertEqual(re.status_code, 200,"接口返回【状态码】:%s ,预期返回【状态码】:%s" % (re.status_code, 200))
-
-        '''请求第一页所有id 和数据库前10条id断言'''
-        self.assertEqual(resid, resdbid, "接口返回所有【id】:%s ,数据库返回所有【id】:%s" % (resid, resdbid))
-
-        '''请求返回数据总数和数据库数据总数断言'''
-        self.assertEqual(total, dbtotal, "接口返回所有【id】:%s ,数据库返回所有【id】:%s" % (total, dbtotal))
 
 
 
