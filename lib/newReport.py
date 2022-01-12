d784e80fbd3c2cea3233c1bc8eb2e20af2a59234
walkerchow <371685029@qq.com>
2022-01-12 17:29:00 +0800

    优化路径拼接

--- lib/newReport.py
+++ lib/newReport.py
@@ -11,6 +11,6 @@ def new_report(testreport):
     :return:返回文件
     """
     lists = os.listdir(testreport)
-    lists.sort(key=lambda fn: os.path.getmtime(testreport + "\\" + fn))
+    lists.sort(key=lambda fn: os.path.getmtime(testreport + "/" + fn))
     file_new = os.path.join(testreport,lists[-1])
     return file_new
