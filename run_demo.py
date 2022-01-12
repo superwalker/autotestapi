feacf846c1df58cbc54c6132233938cbd6f0023c
han <1084952476@qq.com>
2022-01-12 08:25:13 +0000

    Update run_demo.py

--- run_demo.py
+++ run_demo.py
@@ -25,7 +25,7 @@ def run_case(all_case,result_path=setting.TEST_REPORT):
     # test_data.init_data()
 
     now = time.strftime("%Y-%m-%d %H_%M_%S")
-    filename =  result_path + '\' + now + 'result.html'
+    filename =  result_path + '/' + now + 'result.html'
     fp = open(filename,'wb')
     runner = HTMLTestRunner(stream=fp,title='蜜方系统接口自动化测试报告',
                             description='环境：windows 10 浏览器：chrome',
